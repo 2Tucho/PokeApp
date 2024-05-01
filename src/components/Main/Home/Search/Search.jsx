1339977733bb0989f79e8a06771656a7e2162dfe
2Tucho <albergromero@gmail.com>
2024-05-01 20:02:22 +0200

    add pokemonExists conditional

--- src/components/Main/Home/Search/Search.jsx
+++ src/components/Main/Home/Search/Search.jsx
@@ -2,29 +2,40 @@ import { useState, useRef, useEffect } from "react";
 import axios from 'axios'
 import { debounce } from "lodash"; // Librería importada para usar debounce
 
-
 const Search = ({ setPokemonDataList, setPrintPokemon, pokemonDataList }) => {
-  const [pokemonName, setPokemonName] = useState(null);
+  const [pokemonName, setPokemonName] = useState("");
+  const [pokemonExists, setPokemonExists] = useState("")
   const inputRef = useRef();
 
   useEffect(() => { // SI LE QUITO ESTO FUNCIONA BIEN PERO ME HACE PETICIÓN A LA PRIMERA, Y NO QUIERO
-    if(pokemonName) {
+    if (pokemonName == false) {
+      null
+    } else if (pokemonDataList.some(e => e.name == pokemonName)) {
+      console.log("Ese poke ya existe")
+      inputRef.current.value = "";
+      setPokemonExists("El Pokémon que has buscado ya está en la lista. Prueba con otro.")
+    } else {
       async function fetchData() {
         try {
           const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
           const json = res.data;
           console.log(json);
+          console.log(pokemonDataList.some(e => e.name == pokemonName))
           setPrintPokemon(json);
           setPokemonDataList([json, ...pokemonDataList]); //json = {...}
           console.log(pokemonDataList);
           inputRef.current.value = "";
+          /* console.log(pokemonDataList[0].name)
+          console.log(pokemonName)
+          let pokeName = (pokemonDataList.find(name => name.name = pokemonName)).name
+          console.log(pokeName)
+          pokeName == pokemonName ? console.log(true) : console.log(false) */
         } catch {
           console.log("ERROR: NOT FOUND")
         }
       };
-  
       fetchData();
-    } else null
+    }
   }, [pokemonName])
 
   function changePokeName() {
@@ -38,6 +49,7 @@ const Search = ({ setPokemonDataList, setPrintPokemon, pokemonDataList }) => {
     <section>
       <input type="text" onChange={debouncedOnChange} ref={inputRef} />
       <button onClick={changePokeName}>Search</button>
+      <p>{pokemonExists}</p>
     </section>
   );
 };
